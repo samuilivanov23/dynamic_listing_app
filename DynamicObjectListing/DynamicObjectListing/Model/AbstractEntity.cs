27afa4e6529616a0d66c5b904ee5ca502783721f
samuilivanov23 <samuil2001ivanov@gmail.com>
2023-04-20 22:23:19 +0300

    ***(20230420) ***note=Adding Initial View and Components ***feature-minor

---
+++ DynamicObjectListing/DynamicObjectListing/Model/AbstractEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicObjectListing.Model
+{
+    public abstract class AbstractEntity
+    {
+        /// <summary>
+        /// Base Entity that is inherited by other entities which will be listed
+        /// </summary>
+        /// <returns></returns>
+        public abstract override string ToString();
+    }
+}
---
+++ DynamicObjectListing/DynamicObjectListing/Model/Athlete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DynamicObjectListing.Model
+{
+    [Table( "athletes" )]
+    public class Athlete : AbstractEntity
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        public Int64 Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int ActivitiesCount { get; set; }
+
+        public int Followers { get; set; }
+
+        public int Following { get; set; }
+        public override string ToString()
+        {
+            return FirstName + LastName + ", Activities Count: " + ActivitiesCount + ", Followers: " + Followers + ", Following: " + Following;
+        }
+    }
+}
