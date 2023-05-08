00944353954a5130ff9831330b20fb13447cf4f8
samuilivanov23 <samuil2001ivanov@gmail.com>
2023-05-08 21:57:20 +0300

    ***(20230421) ***note=Adding ICreator generic validation ***feature-minor

--- DynamicObjectListing/DynamicObjectListing/InstantiationManager/ICreator.cs
+++ DynamicObjectListing/DynamicObjectListing/InstantiationManager/ICreator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DynamicObjectListing.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DynamicObjectListing.InstantiationManager
 {
-    public interface ICreator<T>
+    public interface ICreator<T> where T : IBaseModel
     {
         bool CanHandle(string type);
         T Create();
